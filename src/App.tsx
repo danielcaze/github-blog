edfb3d05627a617fe7c53693619bbedb635a8e57
Daniel Cazé <danielctlima@gmail.com>
2022-11-10 09:35:41 -0300

    eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import { Router } from "./Router";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,14 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: "Nunito", sans-serif;
+  }
+
+  html, body {
+    font-size: 62.5%;
   }
 `
