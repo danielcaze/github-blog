5bc898d6dcd70aeb60e26f419c380dd8c6699808
Daniel Cazé <danielctlima@gmail.com>
2022-11-19 19:53:08 -0300

    scroll to top function

--- src/layout/default/index.tsx
+++ src/layout/default/index.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
+import { ScrollToTop } from '../../utils/ScrollToTop'
 import * as S from './styles'
 
 export function DefaultLayout() {
   return (
     <S.DefaultLayoutWrapper>
+      <ScrollToTop />
       <Header />
       <S.OutletContainer>
         <Outlet />
